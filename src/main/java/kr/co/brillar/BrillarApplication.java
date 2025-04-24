c5f75562d200e5271878d2a9f0ed733d5d0347f0
Dev.Yem <134784917+ArcherYEM@users.noreply.github.com>
2025-04-24 23:13:59 +0900

    초기파일 세팅

---
+++ src/main/java/kr/co/brillar/BrillarApplication.java
@@ -0,0 +1,13 @@
+package kr.co.brillar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrillarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrillarApplication.class, args);
+    }
+
+}
