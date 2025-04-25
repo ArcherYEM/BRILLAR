8f3d75f8bf41fb40b8ccbcdc803b9c11835b27e6
Dev.Yem <134784917+ArcherYEM@users.noreply.github.com>
2025-04-25 23:55:42 +0900

    메인페이지 html 작업

--- src/main/java/kr/co/brillar/controller/MainController.java
+++ src/main/java/kr/co/brillar/controller/MainController.java
@@ -1,9 +1,12 @@
 package kr.co.brillar.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-public class HomeController {
+@Controller
+public class MainController {
 
+    // 메인페이지 조회
     @GetMapping("/")
     public String index(){
         return "main/index";
