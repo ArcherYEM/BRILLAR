804fbf7d2a7a9e32870b6f036d70af1f2aa1d1b0
유창규 <yhs02252@gmail.com>
2025-05-08 17:13:14 +0900

    내 정보 수정 + 비밀번호 변경 + 주소 변경

--- src/main/java/kr/co/brillar/mapper/UserMapper.java
+++ src/main/java/kr/co/brillar/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package kr.co.brillar.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import kr.co.brillar.dto.UserDto;
 
@@ -8,8 +9,10 @@ import kr.co.brillar.dto.UserDto;
 public interface UserMapper {
     
     UserDto getUser(String userId);
-
+    
     int updateUser(UserDto userDto);
  
     int deleteUser(String userId);
+
+    int updatePassword(@Param("newPassword")String newPassword, @Param("userId") String userId);
 }
