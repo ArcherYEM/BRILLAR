a6ba123ab058f0c0e456d994e33e04d235faeafd
유창규 <yhs02252@gmail.com>
2025-05-31 22:30:37 +0900

    detail material 작업중

--- src/main/java/kr/co/brillar/controller/restAPI/DetailRestAPI.java
+++ src/main/java/kr/co/brillar/controller/restAPI/DetailRestAPI.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 import kr.co.brillar.dto.DetailDto;
 import kr.co.brillar.service.DetailService;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
@@ -21,6 +24,22 @@ public class DetailRestAPI {
         DetailDto dto = detailService.getDetail(productSeq);
         
         return dto;
+    }
+    
+    @GetMapping("/material")
+    public List<DetailDto> getMaterial(int productSeq) {
+
+        List<DetailDto> material = detailService.getMaterial(productSeq);
+        
+        return material;
+    }
+    
+    @GetMapping("/size")
+    public List<DetailDto> getSize(int groupId) {
+
+        List<DetailDto> size = detailService.getSize(groupId);
+
+        return size;
     }
     
 }
--- src/main/java/kr/co/brillar/service/DetailService.java
+++ src/main/java/kr/co/brillar/service/DetailService.java
@@ -1,5 +1,7 @@
 package kr.co.brillar.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import kr.co.brillar.dto.DetailDto;
@@ -17,5 +19,19 @@ public class DetailService {
         DetailDto dto = detailMapper.getProductDetail(productSeq);
         
         return dto;
+    }
+
+    public List<DetailDto> getMaterial(int productSeq){
+
+        List<DetailDto> material = detailMapper.getProductMaterial(productSeq);
+        
+        return material;
+    }
+
+    public List<DetailDto> getSize(int groupId){
+
+        List<DetailDto> size = detailMapper.getProductSize(groupId);
+        
+        return size;
     }
 }
